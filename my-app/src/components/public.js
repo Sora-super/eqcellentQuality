efd8a1665a649333475faf07a62271eb6858ee7b
Administrator <Administrator@PC-20160724FCXS>
2017-05-03 10:41:00 +0800

    1.01

---
+++ my-app/src/components/public.js
@@ -0,0 +1,13 @@
+import React,{Component} form "react";
+
+
+class Header extends Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return <div>{this.props.children}</div>	
+	}
+}
+
+class Content extends Con
