f4f8fe96e3f2e705211de1fe9efa270c6b75b8ff
Administrator <Administrator@PC-20160724FCXS>
2017-05-03 10:13:44 +0800

    1.0

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import Product from './routes/Product/product';
+ReactDOM.render(<Product />
+  ,document.getElementById('root')
+);
---
+++ my-app/src/routes/Product/product.js
@@ -0,0 +1,172 @@
+import React,{Component} from "react";
+import { NavBar, Icon , TabBar,Tabs, WhiteSpace} from 'antd-mobile';
+
+
+const TabPane = Tabs.TabPane;
+
+function callback(key) {
+  console.log(key);
+}
+//选项卡
+const TabExample = () => (
+  <div>
+    <Tabs defaultActiveKey="1" onChange={callback}>
+      <TabPane tab="选项卡一" key="1">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '5rem', backgroundColor: '#fff' }}>
+          选项卡一内容
+        </div>
+      </TabPane>
+      <TabPane tab="选项卡二" key="2">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '5rem', backgroundColor: '#fff' }}>
+          选项卡二内容
+        </div>
+      </TabPane>
+      <TabPane tab="选项卡三" key="3">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '5rem', backgroundColor: '#fff' }}>
+          选项卡三内容
+        </div>
+      </TabPane>
+    </Tabs>
+    <WhiteSpace />
+  </div>
+);
+//底部
+class TabBarExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+    };
+  }
+
+  renderContent(pageText) {
+    return (
+      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+        <div style={{ paddingTop: 60 }}>你已点击“{pageText}” tab， 当前展示“{pageText}”信息</div>
+        <a style={{ display: 'block', marginTop: 40, marginBottom: 600, color: '#108ee9' }} onClick={(e) => {
+          e.preventDefault();
+          this.setState({
+            hidden: !this.state.hidden,
+          });
+        }}
+        >
+          点击切换 tab-bar 显示/隐藏
+        </a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="white"
+        hidden={this.state.hidden}
+      >
+        <TabBar.Item
+          title="生活"
+          key="生活"
+          icon={<div style={{
+            width: '0.44rem',
+            height: '0.44rem',
+            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  0.42rem 0.42rem no-repeat' }}
+          />
+          }
+          selectedIcon={<div style={{
+            width: '0.44rem',
+            height: '0.44rem',
+            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  0.42rem 0.42rem no-repeat' }}
+          />
+          }
+          selected={this.state.selectedTab === 'blueTab'}
+          badge={1}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}
+          data-seed="logId"
+        >
+          {this.renderContent('生活')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon type="koubei-o" size="md" />}
+          selectedIcon={<Icon type="koubei" size="md" />}
+          title="口碑"
+          key="口碑"
+          badge={'new'}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+            });
+          }}
+          data-seed="logId1"
+        >
+          {this.renderContent('口碑')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={
+            <div style={{
+              width: '0.44rem',
+              height: '0.44rem',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  0.42rem 0.42rem no-repeat' }}
+            />
+          }
+          selectedIcon={
+            <div style={{
+              width: '0.44rem',
+              height: '0.44rem',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  0.42rem 0.42rem no-repeat' }}
+            />
+          }
+          title="朋友"
+          key="朋友"
+          dot
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+            });
+          }}
+        >
+          {this.renderContent('朋友')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+          selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+          title="我的"
+          key="我的"
+          selected={this.state.selectedTab === 'yellowTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'yellowTab',
+            });
+          }}
+        >
+          {this.renderContent('我的')}
+        </TabBar.Item>
+      </TabBar>
+    );
+  }
+}
+class Product extends Component {
+	render(){
+		return (
+			<div>
+				 <NavBar leftContent="back" mode="light" onLeftClick={() => console.log('onLeftClick')}
+			      rightContent={[
+			        <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
+			        <Icon key="1" type="ellipsis" />,
+			      ]}
+			    >商品页面</NavBar>
+			    <TabBarExample/>
+			    <TabExample/>
+			</div>
+		)
+	}
+}
+export default Product;
